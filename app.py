a27880ebf15065a701ed5d17f8e2c858ca19077e
PHopson <peter.c.hopson@gmail.com>
2022-02-12 15:27:46 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import random
+import re
+import string
+
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+app = Flask('impossible')
+cors = CORS(app)
+
+#route for generating the MBI
+@app.route('/generate/', methods=['GET'])
+@cross_origin()
+def generate():
+    #Initialize usable characters
+    rangeDigit = list(range(10))
+
+    rangeAlpha = list(string.ascii_uppercase)
+    rangeAlpha.remove("B")
+    rangeAlpha.remove("I")
+    rangeAlpha.remove("L")
+    rangeAlpha.remove("O")
+    rangeAlpha.remove("S")
+    rangeAlpha.remove("Z")
+
+
+    #Build the MBI string
+    resString = ""
+    for i in range(1,12):
+        if i==1:
+            resString += str( random.choice(rangeDigit[1:]) )
+        elif i==2 or i==5 or i==8 or i==9:
+            resString += random.choice(rangeAlpha)
+        elif i==3 or i==6:
+            resString += str( random.choice(rangeDigit+rangeAlpha) )
+        elif i==4 or i==7 or i==10 or i==11:
+            resString += str( random.choice(rangeDigit) )
+
+    return {'MBI':resString}
+
+#route for validation
+@app.route('/verify', methods=['POST'])
+@cross_origin()
+def verify():
+    check = request.get_json()["verify"]
+    #print(check)
+
+    #a is usable alpha characters
+    #an is the combination of digits and alpha characters
+    #n is digits
+    a = "[AC-HJKMNPQRT-Y]"
+    an = "[0-9AC-HJKMNPQRT-Y]"
+    n = "[0-9]"
+    
+    pattMBI = "^[1-9]"+a+an+n+a+an+n+a+a+n+n+"$"
+    
+    if re.match(pattMBI,check):
+        return {'valid':True}
+    else:
+        return {'valid':False}
