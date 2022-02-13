ed9fcd80b5930d13789b3b163642986429ade205
PHopson <peter.c.hopson@gmail.com>
2022-02-12 19:54:38 -0500

    Error handling for verify

--- app.py
+++ app.py
@@ -46,19 +46,22 @@ def generate():
 @app.route('/verify', methods=['POST'])
 @cross_origin()
 def verify():
-    check = request.get_json()["verify"]
-    #print(check)
-
-    #a is usable alpha characters
-    #an is the combination of digits and alpha characters
-    #n is digits
-    a = "[AC-HJKMNPQRT-Y]"
-    an = "[0-9AC-HJKMNPQRT-Y]"
-    n = "[0-9]"
-    
-    pattMBI = "^[1-9]"+a+an+n+a+an+n+a+a+n+n+"$"
-    
-    if re.match(pattMBI,check):
-        return {'valid':True}
-    else:
-        return {'valid':False}
+    reqJSON = request.get_json()
+    if reqJSON is not None:
+        if "verify" in reqJSON:
+            check = reqJSON["verify"]
+            #a is usable alpha characters
+            #an is the combination of digits and alpha characters
+            #n is digits
+            a = "[AC-HJKMNPQRT-Y]"
+            an = "[0-9AC-HJKMNPQRT-Y]"
+            n = "[0-9]"
+            
+            pattMBI = "^[1-9]"+a+an+n+a+an+n+a+a+n+n+"$"
+            
+            if re.match(pattMBI,check):
+                return {'valid':True}
+            else:
+                return {'valid':False}
+        else:
+            return "Expected verify element missing", 422
